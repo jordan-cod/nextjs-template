7fd984428ce942ae555a70b1da6dc7c38c97f9c2
Gabriel Jordan <gabrijordan3@gmail.com>
2025-01-11 18:00:24 -0300

    Add Header and Footer components with basic styling

---
+++ src/components/layout/footer/footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer(): React.ReactNode {
+    return (
+        <footer className="w-full h-20 bg-black text-white p-4">Footer</footer>
+    );
+}
---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,7 @@
+export default function Header(): React.ReactNode {
+    return (
+        <header className="width-full h-20 bg-black text-white p-4 flex justify-between items-center">
+            Header
+        </header>
+    );
+}
