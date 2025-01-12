6959cdef0d7abdce555c569cd480274c3ccc9068
Gabriel Jordan <gabrijordan3@gmail.com>
2025-01-11 22:14:50 -0300

    Add TypeScript definitions for environment variables related to authentication

---
+++ env.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+namespace NodeJS {
+    interface ProcessEnv {
+        AUTH_SECRET: string;
+        AUTH_URL: string;
+        BASE_URL: string;
+        AUTH_DISCORD_ID: string;
+        AUTH_DISCORD_SECRET: string;
+    }
+}
